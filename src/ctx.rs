919d173d4b6d204b663f58c368d282a6e7a6583a
Mokuzzai <pullinen.joona@gmail.com>
2023-01-15 21:35:33 +0200

    remove unneccessary code and define `Context`
    Signed-off-by: Mokuzzai <pullinen.joona@gmail.com>

--- src/ctx.rs
+++ src/ctx.rs
@@ -1,1 +1,30 @@
+//! A 'Context' represents the neighboring chunks blocks which are directly next to our chunk
+//! NOTE: Currently only supported for uniform chunks
+
+use crate::Chunk;
+
+pub struct DiagonalAxisContext<T> {
+	pub near: T,
+	pub far: T,
+}
+
+/// `C`: Chunkspace coordinates
+/// `A`: Area of a single face
+pub struct ParallelAxisContext<T, const A: usize> {
+	pub near: [T; A],
+	pub far: [T; A],
+}
+
+/// `C`: Chunkspace coordinates
+/// `A`: Area of a single face
+pub struct ParallelContext<T, const A: usize, const C: usize> {
+	pub context: [ParallelAxisContext<T, A>; C],
+}
+
+/// `C`: Chunkspace coordinates
+pub struct DiagonalContext<T, const C: usize> {
+	pub context: [DiagonalAxisContext<T>; C],
+}
+
+
 
--- src/position_index_conversion.rs
+++ src/position_index_conversion.rs
@@ -45,32 +45,20 @@ pub fn index_to_position<const B: usize>(
 	Some(out)
 }
 
-pub(crate) fn unroll_extents_range<const B: usize>(
-	range: Range<na::Vector<i32, B>>,
-) -> na::Vector<usize, B> {
-	let extents = range.end - range.start;
-	let extents: na::Vector<usize, B> = extents.try_cast().unwrap();
-	extents
-}
-
-pub fn position_to_index_with_range<const B: usize>(
-	range: Range<na::Vector<i32, B>>,
+pub fn position_to_index_offset<const B: usize>(
+	extents: na::Vector<usize, B>,
+	offset: na::Vector<i32, B>,
 	position: na::Vector<i32, B>,
 ) -> Option<usize> {
-	let offset = range.start;
-
-	position_to_index(unroll_extents_range(range), position + offset)
+	position_to_index(extents, position - offset)
 }
 
-pub fn index_to_position_with_range<const B: usize>(
-	range: Range<na::Vector<i32, B>>,
+pub fn index_to_position_offset<const B: usize>(
+	extents: na::Vector<usize, B>,
+	offset: na::Vector<i32, B>,
 	index: usize,
 ) -> Option<na::Vector<i32, B>> {
-	let offset = range.start;
-
-	let ret = index_to_position(unroll_extents_range(range), index)?;
-
-	Some(ret + offset)
+	index_to_position(extents, index).map(|position| position + offset)
 }
 
 #[cfg(test)]
@@ -116,82 +104,5 @@ mod origin {
 
 			assert_eq!(expected.cast(), result);
 		})
-	}
-	mod with_range {
-		use super::*;
-		#[test]
-		fn test_position_to_index_with_range_0() {
-			helper(|expected, position| {
-				let result = position_to_index_with_range(
-					na::Vector::from([0; 5])..na::Vector::from([X, Y, Z, W, V]).cast(),
-					position.cast(),
-				)
-				.unwrap();
-
-				assert_eq!(expected as usize, result);
-			})
-		}
-		#[test]
-		fn test_position_to_index_with_range_1() {
-			helper(|expected, position| {
-				let result = position_to_index_with_range(
-					na::Vector::from([0; 5])..na::Vector::from([X, Y, Z, W, V]).cast(),
-					position.cast(),
-				)
-				.unwrap();
-
-				assert_eq!(expected as usize, result);
-			})
-		}
-		#[test]
-		fn test_index_to_position_with_range() {
-			let mut it = 0..C;
-
-			for v in 0..V {
-				for w in 0..W {
-					for z in 0..Z {
-						for y in 0..Y {
-							for x in 0..Y {
-								let expected = na::Vector::from([x, y, z, w, v]);
-								let result = index_to_position_with_range(
-									na::Vector::from([0; 5])..na::Vector::from([X, Y, Z, W, V]).cast(),
-									it.next().unwrap() as usize,
-								)
-								.unwrap();
-
-								assert_eq!(expected.cast(), result);
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	mod unroll_extents {
-		use super::*;
-
-		#[test]
-		fn start_0() {
-			assert_eq!(
-				na::Vector::from([5, 1, 3]),
-				unroll_extents_range(na::Vector::from([0; 3])..na::Vector::from([5, 1, 3]))
-			);
-		}
-
-		#[test]
-		fn start_negative_1() {
-			assert_eq!(
-				na::Vector::from([6, 2, 4]),
-				unroll_extents_range(na::Vector::from([-1; 3])..na::Vector::from([5, 1, 3]))
-			);
-		}
-
-		#[test]
-		fn start_positive_1() {
-			assert_eq!(
-				na::Vector::from([4, 0, 2]),
-				unroll_extents_range(na::Vector::from([1; 3])..na::Vector::from([5, 1, 3]))
-			);
-		}
 	}
 }
